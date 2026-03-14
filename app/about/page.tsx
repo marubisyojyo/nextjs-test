d8169470e04cc5d70989d727f2eb443af25ea37f
pcar <pasokonar@gmail.com>
2026-03-14 19:39:35 +0900

    update

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main style={{padding:"80px"}}>
+      <h1>About</h1>
+      <p>This is my first Next.js website.</p>
+    </main>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <main style={{padding:"80px"}}>
+      <h1>Contact</h1>
+      <p>Contact me here.</p>
+    </main>
+  );
+}
