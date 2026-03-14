dfb6052fb09f22c3f01737dd57b3aa51c05b34d4
pcar <pasokonar@gmail.com>
2026-03-14 19:32:26 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
 export default function Home() {
   return (
-    <main>
-      <h1 style={{fontSize:"60px"}}>MY FIRST WEBSITE</h1>
-      <p>Welcome to my site</p>
+    <main style={{textAlign:"center", padding:"80px"}}>
+      
+      <h1 style={{fontSize:"60px"}}>
+        My First Website
+      </h1>
+
+      <p style={{fontSize:"20px", marginTop:"20px"}}>
+        Next.jsで作った最初のサイトです
+      </p>
+
+      <button
+        style={{
+          marginTop:"40px",
+          padding:"15px 30px",
+          fontSize:"18px",
+          background:"#0070f3",
+          color:"white",
+          border:"none",
+          borderRadius:"10px",
+          cursor:"pointer"
+        }}
+      >
+        Get Started
+      </button>
+
     </main>
   );
 }
